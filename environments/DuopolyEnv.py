b637faf8ed5ac22b32b6b58dd2d14fa53434c649
abyaadrafid <abyaadrafid@gmail.com>
2023-09-18 18:40:32 +0200

    Add RLLib compatibility helpers
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- environments/DuopolyEnv.py
+++ environments/DuopolyEnv.py
@@ -11,17 +11,18 @@ from utils.market import Market
 
 
 class DuopolyEnv(MultiAgentEnv, gym.Env):
-    def __init__(self, config: Optional[Dict]) -> None:
+    def __init__(self, seed: Optional[int], config: Optional[Dict] = None) -> None:
+        super().__init__()
         if not config:
             config = {}
 
         # RLLib Compatibility
-        action_space = None
-        observation_space = None
-
         self.parse_config(config)
         self._init_spaces()
         self._init_states()
+
+    def seed(self, seed: int):
+        self.seed = seed
 
     def _init_spaces(self):
 
@@ -37,15 +38,18 @@ class DuopolyEnv(MultiAgentEnv, gym.Env):
             low=self.min_price,
             high=self.max_price,
             shape=(self.memory_size, self.num_seller),
-            dtype="uint8",
+            dtype="int16",
         )
         self.n_features = self.memory_size * self.num_seller
+        self._agent_ids = ["agent" + str(i) for i in range(self.num_seller)]
 
     def _init_states(self):
 
         # Generate random state
-        prices = np.random.uniform(
-            self.min_price, self.max_price, (self.memory_size, self.num_seller)
+        prices = np.random.randint(
+            low=self.min_price,
+            high=self.max_price,
+            size=(self.memory_size, self.num_seller),
         )
 
         """
@@ -71,25 +75,40 @@ class DuopolyEnv(MultiAgentEnv, gym.Env):
         self.max_price = config.get("max_price", 1200)
         self.min_price = config.get("min_price", 500)
         self.num_seller = config.get("num_seller", 2)
-        self.max_capacity = config.get("max_capacity", 2000)
+        self.max_capacity = config.get("max_capacity", 1000)
         self.memory_size = config.get("memory_size", 5)
+        self.seller_ids = config.get("agent_ids", ["agent0", "agent1"])
         self.action_type = config.get(
             "action_type", "Discrete"
         )  # can be continous (not super good for DQN)
 
     @override(gym.Env)
-    def step(self, actions: list[int]):
-        self._create_states(actions)
-        revenue = np.array(self.market.allocate_items(actions))
+    def step(self, actions: Dict):
+        print(actions)
 
-        # My understanding : large reward signals arent usually the best thing
-        rewards = normalize(revenue)
-        return self.states, rewards, False, {}  # next_state, rewards, dones, info
+        if actions:
+            actions = self._from_RLLib_API_to_list(actions)
+            print(actions)
+
+            self._create_states(actions)
+            revenue = np.array(self.market.allocate_items(actions))
+
+            # My understanding : large reward signals arent usually the best thing
+            rewards = normalize(revenue)
+            return (
+                self._build_observation_dictionary(),
+                rewards,
+                False,
+                False,
+                {},
+            )  # next_state, rewards, dones, info
+        else:
+            return self._build_observation_dictionary(), 0, False, False, {}
 
     @override(gym.Env)
-    def reset(self):
+    def reset(self, *, seed=None, options=None):
         self._init_states()
-        return self.states
+        return self._build_observation_dictionary(), {}
 
     @override(gym.Env)
     def close(self):
@@ -98,3 +117,39 @@ class DuopolyEnv(MultiAgentEnv, gym.Env):
     @override(gym.Env)
     def render(self):
         pass
+
+    def _build_observation_dictionary(self):
+        """
+        Create dictonary of PlayerID:Observation for RLLib support
+        """
+
+        observation = {}
+        for i in range(self.num_seller):
+            observation[self.seller_ids[i]] = self.states
+
+        return observation
+
+    def _from_RLLib_API_to_list(self, actions):
+        """
+        Parse RLLib dictonary into lists
+        """
+        actions = [actions[player_id] for player_id in self.seller_ids]
+        return actions
+
+    def _to_RLLib_API(self, observations, rewards, ep_is_done, info):
+        """
+        Turn our list of observations and rewards into dictoraries
+        """
+
+        states_list = {}
+        rewards_list = {}
+        dones_list = {}
+
+        for i in range(self.NUM_AGENTS):
+            states_list[self.player_ids[i]] = observations[i]
+            rewards_list[self.player_ids[i]] = rewards[i]
+            dones_list[self.player_ids[i]] = ep_is_done
+
+        dones_list["__all__"] = ep_is_done
+
+        return states_list, rewards_list, dones_list, info
--- main.py
+++ main.py
@@ -4,50 +4,43 @@ sys.path.append("../")
 
 import ray
 from ray import tune
-from ray.air.config import RunConfig, ScalingConfig
+from ray.air.config import RunConfig
+from ray.rllib.algorithms.dqn import DQNConfig
+from ray.rllib.core.rl_module.marl_module import MultiAgentRLModuleSpec
+from ray.rllib.core.rl_module.rl_module import SingleAgentRLModuleSpec
 from ray.rllib.policy.policy import PolicySpec
 from ray.train.rl import RLTrainer
+from ray.tune.registry import register_env
 
 from environments.DuopolyEnv import DuopolyEnv
 
 
 def main():
     ray.init()
-    rllib_config = {
-        "multiagent": {
-            "policies": {
-                # Use the PolicySpec namedtuple to specify an individual policy:
-                "agent0": PolicySpec(
-                    policy_class=None,  # infer automatically from Algorithm
-                    observation_space=DuopolyEnv(
-                        {}
-                    ).observation_space,  # infer automatically from env
-                    action_space=DuopolyEnv(
-                        {}
-                    ).action_space,  # infer automatically from env
-                    config={"gamma": 0.85},
-                ),
-                "agent1": PolicySpec(
-                    policy_class=None,  # infer automatically from Algorithm
-                    observation_space=DuopolyEnv(
-                        {}
-                    ).observation_space,  # infer automatically from env
-                    action_space=DuopolyEnv(
-                        {}
-                    ).action_space,  # infer automatically from env
-                    config={"gamma": 0.85},
-                ),
+    register_env("duopoly_env", lambda _: DuopolyEnv({}))
+    rllib_config = (
+        DQNConfig()
+        .environment("duopoly_env", disable_env_checking=False)
+        .framework("torch")
+        .multi_agent(
+            policies={
+                "policy": PolicySpec(config=DQNConfig.overrides(framework_str="torch")),
             },
-            "policy_mapping_fn": lambda agent_id, **kwargs: agent_id,
-        },
-    }
-    trainer = RLTrainer(
-        run_config=RunConfig(stop={"training_iteration": 5}),
-        scaling_config=ScalingConfig(num_workers=2, use_gpu=False),
-        algorithm="DQN",
-        config=rllib_config,
+            policy_mapping_fn=lambda agent_id, *args, **kwargs: "policy",
+            policies_to_train=["policy"],
+        )
+        .rl_module(
+            rl_module_spec=MultiAgentRLModuleSpec(
+                module_specs={"policy": SingleAgentRLModuleSpec()}
+            ),
+        )
     )
-    trainer.fit()
+    stop = {"training_iteration": 5}
+    results = tune.Tuner(
+        "DQN",
+        param_space=rllib_config.to_dict(),
+        run_config=RunConfig(stop=stop, verbose=1),
+    ).fit()
 
 
 if __name__ == "__main__":
