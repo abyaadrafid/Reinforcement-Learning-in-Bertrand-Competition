a1ffa6e4af1828ffe781e062ed3264de1b3fad74
abyaadrafid <abyaadrafid@gmail.com>
2023-09-25 04:12:38 +0200

    Remove un-needed callbacks for logger
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- loggers/action_logger.py
+++ loggers/action_logger.py
@@ -13,27 +13,25 @@ import wandb
 
 
 class ActionLogger(DefaultCallbacks):
-    def __init__(self, legacy_callbacks_dict: Dict[str, Any] = None):
-        wandb.init()
-        self.price_table = wandb.Table(columns=["price_value_0", "price_value_1"])
+    def __init__(
+        self, price_table: wandb.Table, legacy_callbacks_dict: Dict[str, Any] = None
+    ):
+        self.price_table = price_table
         super().__init__(legacy_callbacks_dict)
 
-    def on_episode_step(
+    def on_episode_end(
         self,
         *,
         worker: RolloutWorker,
         base_env: BaseEnv,
-        policies: Dict[PolicyID, Policy] | None = None,
-        episode: Episode | EpisodeV2,
+        policies: Dict[PolicyID, Policy],
+        episode: Episode | EpisodeV2 | Exception,
         env_index: int | None = None,
         **kwargs
     ) -> None:
         envs = base_env.get_sub_environments()
-        last_actions = envs[env_index].last()
-        print(last_actions)
-        # self.price_table.add_data([last_actions[i].astype(float) for i in range(2)])
-
-        return super().on_episode_step(
+        self.price_table.add_data(*envs[env_index].get_mean_prices())
+        return super().on_episode_end(
             worker=worker,
             base_env=base_env,
             policies=policies,
@@ -41,8 +39,3 @@ class ActionLogger(DefaultCallbacks):
             env_index=env_index,
             **kwargs
         )
-
-    def on_train_result(self, *, algorithm: Algorithm, result: dict, **kwargs) -> None:
-        wandb.log({"action_list": self.price_table})
-
-        return super().on_train_result(algorithm=algorithm, result=result, **kwargs)
