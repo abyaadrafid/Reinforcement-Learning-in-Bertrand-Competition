5bc7871b8447897addc25d79e369136d9dad5a25
abyaadrafid <abyaadrafid@gmail.com>
2023-09-28 20:32:51 +0200

    Create a remote ray actor to collect actions across all the workers
    Reason : the prices are getting logged across multiple workers in parallel.
    We need this remote actor to collect all the custom metrics and append them.
    
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- loggers/action_logger.py
+++ loggers/action_logger.py
@@ -1,6 +1,6 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict
 
-from ray.rllib.algorithms.algorithm import Algorithm
+import ray
 from ray.rllib.algorithms.callbacks import DefaultCallbacks
 from ray.rllib.env.base_env import BaseEnv
 from ray.rllib.evaluation import RolloutWorker
@@ -12,13 +12,20 @@ from ray.rllib.utils.typing import PolicyID
 import wandb
 
 
-class ActionLogger(DefaultCallbacks):
-    def __init__(
-        self, price_table: wandb.Table, legacy_callbacks_dict: Dict[str, Any] = None
-    ):
-        self.price_table = price_table
-        super().__init__(legacy_callbacks_dict)
+# Create a Ray actor to store shared metrics data
+@ray.remote(name="shared_metrics")
+class SharedMetrics:
+    def __init__(self):
+        self.metrics = []
 
+    def append(self, metric_data):
+        self.metrics.append(metric_data)
+
+    def get_result(self):
+        return self.metrics
+
+
+class ActionLogger(DefaultCallbacks):
     def on_episode_end(
         self,
         *,
@@ -29,8 +36,10 @@ class ActionLogger(DefaultCallbacks):
         env_index: int | None = None,
         **kwargs
     ) -> None:
+        shared_metrics_actor = ray.get_actor("shared_metrics")
         envs = base_env.get_sub_environments()
-        self.price_table.add_data(*envs[env_index].get_mean_prices())
+        mean_prices = envs[env_index].get_mean_prices()
+        shared_metrics_actor.append.remote({"ep_mean_price": mean_prices})
         return super().on_episode_end(
             worker=worker,
             base_env=base_env,
