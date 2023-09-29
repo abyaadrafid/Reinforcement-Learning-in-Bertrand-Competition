b6af94fa72a1947ce511e060faee7d9233be8ccf
abyaadrafid <abyaadrafid@gmail.com>
2023-09-29 02:11:54 +0200

    Update run script with fixed env and added docs
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 
+import ray
+
+import wandb
+
 sys.path.append("../")
 
-import ray
-from ray import tune
 from ray.air.config import RunConfig, ScalingConfig
 from ray.air.integrations.wandb import WandbLoggerCallback
 from ray.rllib.algorithms.a2c import A2CConfig
@@ -11,17 +13,21 @@ from ray.rllib.policy.policy import PolicySpec
 from ray.train.rl import RLTrainer
 from ray.tune.registry import register_env
 
-import wandb
 from environments.DuopolyEnv import DuopolyEnv
 from loggers.action_logger import ActionLogger, SharedMetrics
 
+# config to be passed into the environment
+# WILL BE MOVED TO CONFIG FILES LATER WITH HYDRA SUPPORT
 env_config = {"max_price": 900, "min_price": 500, "memory_size": 5, "num_seller": 2}
 
-
+# creating a dummy env to supply the policy builders with the obs and action spaces
 def env_creator(env_config):
     return DuopolyEnv(env_config)
 
 
+# general run configs
+# MOST WILL BE MOVED TO CONFIG
+# POLICYSPEC WILL HAVE ITS OWN BUILDER BASED ON THE ALGO CHOICE
 config = {
     "env": "duopoly_env",
     "num_workers": 2,
@@ -47,26 +53,52 @@ config = {
 
 def main():
     ray.init()
-    wandb.init(project="RLAC_CUSTOM_METRICS")
+
+    # initializing wandb
+    """
+    group naming convention : envtype_algo_symmetrical_numsteps_numiterations
+    envtype : D = Duopoly
+              O = Oligolpoly
+              M = Monopoly
+    numsteps = Number of steps to run for each episode.
+               In classical setting we would want this to run infinitely
+    """
+    # NAMES WILL BE SET FROM CONFIG
+    wandb.init(project="RLAC_CUSTOM_METRICS", group="D_A2C_SYM_500_500")
+
+    # Initializing a ray remote actor to aggregate data stored in custom callbacks
     shared_metrics_actor = SharedMetrics.remote()
+
+    # Registering env with RLLib
     env_creator(env_config=env_config)
     register_env("duopoly_env", env_creator)
+
+    # Set up trainer
     trainer = RLTrainer(
         run_config=RunConfig(
-            stop={"training_iteration": 2},
-            callbacks=[WandbLoggerCallback(project="TEST_RLAC")],
+            # THIS WILL BE SET FROM CONFIG
+            stop={"training_iteration": 500},
+            callbacks=[WandbLoggerCallback(project="RLAC_CUSTOM_METRICS")],
         ),
         scaling_config=ScalingConfig(num_workers=1, use_gpu=False),
+        # THIS WILL BE SET FROM CONFIG
         algorithm="A2C",
         config=config,
     )
     result = trainer.fit()
+
+    # MOVE THE COLLECTION AND LOGGING TO CORRESPONDING MODULE
+    # Collect data from custom callbacks
     shared_metrics = ray.get(shared_metrics_actor.get_result.remote())
+
+    # Make wandb table
     price_table = wandb.Table(columns=["mean_price_agent_0", "mean_price_agent_1"])
 
+    # Iterate through the list of metrics and add to table
     for ep_metric in shared_metrics:
         price_table.add_data(*ep_metric.get("ep_mean_price"))
 
+    # Send it to wandb
     wandb.log({"Price Table": price_table})
 
 
