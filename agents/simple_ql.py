9b0ef6c1abb47e68c0a474f6fc32dea50649eb77
abyaadrafid <abyaadrafid@gmail.com>
2023-12-12 23:57:41 +0100

    Add comments to simple ql
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- agents/simple_ql.py
+++ agents/simple_ql.py
@@ -15,6 +15,9 @@ class QLearner:
         self._init_q_table()
 
     def _init_q_table(self):
+        # Q table size = discrete actions * discrete observations
+        # Convert observation space into chunks of our choosing
+
         DISCRETE_OS_SIZE = [6] * len(self.observation_space.high)
         self.discrete_os_win_size = (
             self.observation_space.high - self.observation_space.low
@@ -24,12 +27,14 @@ class QLearner:
         )
 
     def _discretize_state(self, state):
+        # Discretizing state (e.g. 1.908 => 5)
         discrete_state = (
             state - self.observation_space.low
         ) / self.discrete_os_win_size
         return tuple(discrete_state.astype(np.int32))
 
     def act(self, state, eps=0.0):
+        # Act in the environment
         rnd = random.random()
         if rnd < eps:
             return np.random.randint(self.action_size)
@@ -39,16 +44,21 @@ class QLearner:
 
     def learn(self, experience):
         state, action, reward, next_state, done = experience
+
+        # update q table if episode ends
         if done:
             max_future_q = np.max(self.q_table[next_state])
             current_q = self.q_table[state + (action,)]
+            # Q update
             new_q = (1 - LR) * current_q + LR * (reward + GAMMA * max_future_q)
             self.q_table[state + (action,)] = new_q
         else:
             self.q_table[state + (action,)] = 0
 
     def step(self, state, action, reward, next_state, done):
+        # Agent step after environment step
         state = self._discretize_state(state)
         next_state = self._discretize_state(next_state)
         self.timestep += 1
+        # learning
         self.learn((state, action, reward, next_state, done))
