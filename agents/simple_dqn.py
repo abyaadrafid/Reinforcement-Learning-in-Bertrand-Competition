1ff90f141d0d30193123d6b188011c3675c71b6b
abyaadrafid <abyaadrafid@gmail.com>
2024-01-12 10:51:41 +0100

    Finish implementing DQN for the average reward setting
    1. Move average reward DQN to its own class
    2. Fix the expected reward calculation and update for average DQN
    3. Do away with optimizer for avg reward and use simple update rule with a step size
    4. Add docstrings and comments
    
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- agents/simple_dqn.py
+++ agents/simple_dqn.py
@@ -14,12 +14,15 @@ import torch.optim as optim
 
 from agents.base_agent import BaseAgent
 
-TAU = 1e-3
-LR = 1e-5
+# Fixed network update parameter
+TAU = 1  # 1 ==> Hard update
+LR = 0.001
 GAMMA = 0.99
-UPDATE_EVERY = 4
-BUFFER_SIZE = int(1e6)
-BATCH_SIZE = 64
+UPDATE_EVERY = 100
+BUFFER_SIZE = int(5000)
+BATCH_SIZE = 32
+# Update step size for average reward estimation
+REWARD_LR = 0.001
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -116,12 +119,16 @@ class ReplayMemory:
 
 
 class DQN(BaseAgent):
+    """
+    Classical Double DQN with a fixed target network for discounted reward setting
+    """
+
     # Deep Q Network algorithm
-    def __init__(self, id, state_size, fc1_size, fc2_size, action_size, type, seed=0):
+    def __init__(self, id, obs_space, fc1_size, fc2_size, action_space, seed=0):
         # fc1_size and fc2_size are linear layer sizes
         self.id = id
-        self.state_size = state_size
-        self.action_size = action_size
+        self.state_size = obs_space.shape[0]
+        self.action_size = action_space.n
         self.seed = random.seed(seed)
 
         self.q_network = DQN_Network(
@@ -132,30 +139,23 @@ class DQN(BaseAgent):
         ).to(device)
 
         # Init optimizer
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=LR)
+        self.optimizer = optim.Adam(
+            self.q_network.parameters(), lr=LR, betas=[0.9, 0.999], eps=1e-7
+        )
 
         # Replay Memory
         self.memory = ReplayMemory(BUFFER_SIZE, BATCH_SIZE, seed)
         self.timestep = 0
-        self.type = type
-
-        # Support for Average reward setting
-        if self.type == "avg_reward":
-            self.avg_rewards = torch.randn(
-                size=(BATCH_SIZE, 1), requires_grad=True, device="cuda"
-            )
-            self.reward_optim = optim.Adam([self.avg_rewards])
 
     def step(self, state, action, reward, next_state, done):
         # Agent step after environment step
         self.memory.add_experience(state, action, reward, next_state, done)
         self.timestep += 1
-        if self.timestep % UPDATE_EVERY == 0:
-            if len(self.memory) > BATCH_SIZE:
-                # Sample from memory
-                sampled_experience = self.memory.sample()
-                # Learn from samples
-                self._learn(sampled_experience)
+        if len(self.memory) > BATCH_SIZE:
+            # Sample from memory
+            sampled_experience = self.memory.sample()
+            # Learn from samples
+            self._learn(sampled_experience)
 
     def _learn(self, experiences):
         # Unpack experiences
@@ -170,30 +170,24 @@ class DQN(BaseAgent):
         max_action_values = action_values.max(1)[0].unsqueeze(1)
 
         # update for average setting
-        if self.type == "avg_reward":
-            Q_target = rewards - self.avg_rewards + max_action_values
-        # update for discounted setting
-        else:
-            Q_target = rewards + GAMMA * max_action_values * (1 - dones)
+
+        Q_target = rewards + GAMMA * max_action_values * (1 - dones)
 
         # Calculate loss
         loss = F.mse_loss(Q_output, Q_target)
 
         # Set zero grad
-        if self.type == "avg_reward":
-            self.reward_optim.zero_grad()
         self.optimizer.zero_grad()
 
         # backprop
         loss.backward()
 
         # optimizer step
-        if self.type == "avg_reward":
-            self.reward_optim.step()
         self.optimizer.step()
 
         # Update fixed target network
-        self._update_target_network(self.q_network, self.target_network)
+        if self.timestep % UPDATE_EVERY == 0:
+            self._update_target_network(self.q_network, self.target_network)
 
     def _update_target_network(self, source_network, target_network):
         for source_parameters, target_parameters in zip(
@@ -220,6 +214,89 @@ class DQN(BaseAgent):
             # Get action from agent
             action = np.argmax(action_values.cpu().data.numpy())
             return action
+
+
+class AvgDQN(DQN):
+    """
+    DQN implementation for average reward setting, applicable for continuous environments
+    This version does not use discounted future rewards, rather uses the expectation of the future rewards
+    """
+
+    def __init__(self, id, obs_space, fc1_size, fc2_size, action_space, seed=0) -> None:
+        super().__init__(id, obs_space, fc1_size, fc2_size, action_space, seed)
+
+        # Randomly initialize average rewards (expected rewards)
+        self.avg_rewards = torch.randn(
+            size=(BATCH_SIZE, 1), requires_grad=True, device=device
+        )
+
+    def step(self, state, action, reward, next_state, done):
+        # Keep the current experience for updating average rewards
+        current_experience = (state, action, reward, next_state)
+        # Add to replay buffer
+        self.memory.add_experience(state, action, reward, next_state, done)
+        self.timestep += 1
+        if len(self.memory) > BATCH_SIZE:
+            # Sample from memory
+            sampled_experience = self.memory.sample()
+            # Learn from samples
+            self._learn(sampled_experience, current_experience)
+
+    def _learn(self, sampled_experience, current_experience):
+        # Update average reward estimation
+        self._update_avg_reward(current_experience)
+        # Unpack
+        states, actions, rewards, next_states, _ = sampled_experience
+
+        # Q output from network
+        Q_output = self.q_network(states).gather(1, actions)
+
+        # Q output from target network
+        action_values = self.target_network(next_states).detach()
+        # Choose actions for max
+        max_action_values = action_values.max(1)[0].unsqueeze(1)
+
+        # update for average setting
+
+        Q_target = rewards - self.avg_rewards + max_action_values
+
+        # Calculate loss
+        loss = F.mse_loss(Q_output, Q_target)
+
+        # Set zero grad
+        self.optimizer.zero_grad()
+
+        # backprop
+        loss.backward()
+
+        # optimizer step
+        self.optimizer.step()
+
+        # Update fixed target network
+        if self.timestep % UPDATE_EVERY == 0:
+            self._update_target_network(self.q_network, self.target_network)
+
+    def _update_avg_reward(self, current_experience):
+
+        # Unpacking and converting to tensor
+        state, action, reward, next_state = current_experience
+        state = torch.tensor(state).float()
+        action = torch.tensor(action)
+        reward = torch.tensor(reward).float()
+        next_state = torch.tensor(next_state).float()
+
+        with torch.no_grad():
+            # Q output from Q network for the current sample
+            Q_output = self.q_network(state).gather(0, action)
+
+            # Q output from target network
+            action_values = self.target_network(next_state).detach()
+            # Choose actions for max
+            max_action_values = action_values.max()
+            # Make bootstrap update on average rewards
+            self.avg_rewards = self.avg_rewards + REWARD_LR * (
+                reward - self.avg_rewards + max_action_values - Q_output
+            )
 
 
 class DQN_Network(nn.Module):
