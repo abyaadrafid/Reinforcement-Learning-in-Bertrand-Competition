5e148d58f7bb35a8be2d4cc2fd158b4ec01359d9
abyaadrafid <abyaadrafid@gmail.com>
2024-01-21 21:16:30 +0100

    Add support for collecting metrics and losses
    Signed-off-by: abyaadrafid <abyaadrafid@gmail.com>

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,10 +1,17 @@
 class BaseAgent:
     def __init__(self, id) -> None:
         self.id = id
-        self.losses = None
+        self.losses = {}
+        self.metrics = {}
 
     def act(self, state, eps=0.0):
         pass
 
     def step(self, state, action, reward, next_state, done):
         pass
+
+    def get_metrics(self):
+        return self.metrics
+
+    def get_losses(self):
+        return self.losses
